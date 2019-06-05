b4098907a5ca767448e875d6ac3e0d722a4048a1
sujanpaul <ksujanpaul32@gmail.com>
2019-06-05 18:56:09 +0530

    latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+ 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+ 
+import { IonicStorageModule } from '@ionic/storage';
+import { HttpModule } from '@angular/http';
+ 
+import { DatabaseProvider } from '../providers/database/database';
+ 
+import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
+import { SQLite } from '@ionic-native/sqlite/ngx';
+ 
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +22,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    IonicStorageModule.forRoot(),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +34,10 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DatabaseProvider,
+    SQLitePorter,
+    SQLite
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
+import { DatabaseProvider } from './../../providers/database/database';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, Platform } from 'ionic-angular';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  developer = {};
+  developers = [];
+ 
+  constructor(public navCtrl: NavController, private databaseprovider: DatabaseProvider, private platform: Platform) {
+    this.databaseprovider.getDatabaseState().subscribe(rdy => {
+      if (rdy) {
+        this.loadDeveloperData();
+      }
+    })
   }
-
+ 
+  loadDeveloperData() {
+    this.databaseprovider.getAllDevelopers().then(data => {
+      this.developers = data;
+    })
+  }
+ 
+  addDeveloper() {
+    this.databaseprovider.addDeveloper(this.developer['name'], this.developer['skill'], parseInt(this.developer['yearsOfExperience']))
+    .then(data => {
+      this.loadDeveloperData();
+    });
+    this.developer = {};
+  }
+ 
 }
